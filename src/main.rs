a5c2b7523ab7ff66220a12cf9a4e49f74005f59f
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2024-01-31 12:42:02 +0000

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,11 @@ fn main() {
     let spdc = spd.clone();
 
     // let rt = tokio::runtime::Runtime::new().unwrap();
-    let rt = tokio::runtime::Builder::new_current_thread().enable_io().enable_time().build().unwrap();
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_io()
+        .enable_time()
+        .build()
+        .unwrap();
 
     rt.block_on(async {
         let listen = format!("{}:{}", args.ip, args.port);
